9e2ac16553d03561cab0af8f34b58133046f4b46
Jona Strickler <jona.strickler@web.de>
2023-01-30 17:16:04 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import "./fontface.css";
 import "./App.css";
-import Location from "./screens/Location";
-import Settings from "./screens/Settings";
 import Home from "./screens/Home"
 
 function App() {
 	return (
 		<Home />
-		// <Settings />
-		// <Location />
 	);
 }
 
