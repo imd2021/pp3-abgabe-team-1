5f4c3d4bdff470f9898b7743577cd28f20cf21f7
Jona Strickler <jona.strickler@web.de>
2023-01-31 12:35:03 +0100

    firebase config file

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
-import { useEffect, useState, createContext } from "react";
+import { useEffect, useState } from "react";
 import "./fontface.css";
 import "./App.css";
 import Home from "./screens/Home";
 import { ScreenOrientation } from "@ionic-native/screen-orientation";
 
-import { initializeApp } from "firebase/app";
-import {
-	doc,
-	addDoc,
-	deleteDoc,
-	getFirestore,
-	orderBy,
-	serverTimestamp,
-	collection,
-	query,
-	onSnapshot,
-} from "firebase/firestore";
-import { create } from "domain";
-
-const firebaseConfig = {
-	apiKey: "AIzaSyCLcw8uUMz5lzkW-y7TCspNHdvY-gB6yC4",
-	authDomain: "bob-p3.firebaseapp.com",
-	projectId: "bob-p3",
-	storageBucket: "bob-p3.appspot.com",
-	messagingSenderId: "680370630200",
-	appId: "1:680370630200:web:fd299edbca4a122ae800cd",
-	measurementId: "G-NZ2KN9TW3G",
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const emergenciesRef = collection(db, "emergencies");
+import { onSnapshot } from "firebase/firestore";
+import emergenciesRef from "./firebaseConfig";
 
 function App() {
 	const [calls, setCalls] = useState([]);
 
+	// lists all emergency calls from the db collection if it changes
 	useEffect(() => {
 		const unsubscribe = onSnapshot(emergenciesRef, (snapshot) => {
 			setCalls(
@@ -51,10 +27,10 @@ function App() {
 		};
 	}, []);
 
-	// // locks app into portrait mode
-	// useEffect(() => {
-	// 	ScreenOrientation.lock(ScreenOrientation.ORIENTATIONS.PORTRAIT);
-	// }, []);
+	// locks app into portrait mode
+	useEffect(() => {
+		ScreenOrientation.lock(ScreenOrientation.ORIENTATIONS.PORTRAIT);
+	}, []);
 
 	return (
 			<Home calls={calls}/>
---
+++ src/firebaseConfig.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import {
+	getFirestore,
+	collection,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyCLcw8uUMz5lzkW-y7TCspNHdvY-gB6yC4",
+	authDomain: "bob-p3.firebaseapp.com",
+	projectId: "bob-p3",
+	storageBucket: "bob-p3.appspot.com",
+	messagingSenderId: "680370630200",
+	appId: "1:680370630200:web:fd299edbca4a122ae800cd",
+	measurementId: "G-NZ2KN9TW3G",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const emergenciesRef = collection(db, "emergencies");
+
+export default emergenciesRef;
